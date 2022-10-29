293231e9cbcd9c435a562d850f88163f29101af4
Henrique Sander Lourenço <henrique.sander@i-sensi.io>
2022-10-29 19:54:57 -0300

    code working

--- main.py
+++ main.py
@@ -7,33 +7,28 @@ def fact(x):
     return x * fact(x - 1) if x > 1 else 1
 
 def fib(x):
-    return fib(x - 1) + (x - 2) if x > 1 else 1 if x == 1 else 0
+    return fib(x - 1) + fib(x - 2) if x > 1 else 1 if x == 1 else 0
 
-def isNumber(element):
-    return True if (element != '\n') and (element != ' ') else False
+def isNumber(listItem):
+    return True if (listItem != '\n') and (listItem != ' ') else False
 
-def separateELements(numList):
+def indexIsEven(listItem):
+    return True if (listItem[0] % 2 == 0) else False
 
-with open('input.dat') as inputFile:
+def indexIsOdd(listItem):
+    return True if (listItem[0] % 2 != 0) else False
+
+with open('input.dat', 'r') as inputFile:
     inputData = list(inputFile.read())
-    filteredInputData = list(filter(isNumber, inputData))
-    numberInputData =  list(map(int, filteredInputData))
-    print(filteredInputData)
 
-#for element in filteredInputData:
-#    if i % 2 == 0:
-#        if 'thisList'
+filteredInputData = list(filter(isNumber, inputData))
+numberInputData =  list(enumerate(map(int, filteredInputData)))
+evenIndexData = [data[1] for data in list(filter(indexIsEven, numberInputData))]
+oddIndexData = [data[1] for data in list(filter(indexIsOdd, numberInputData))]
+fibonacci = list(map(fib, evenIndexData))
+factorial = list(map(fact, oddIndexData))
+outputLines = ["Linha {}:\tfibonacci: {}\tfactorial: {}\n".format(i, fibonacci[i], factorial[i]) for i in range(len(fibonacci))]
+outputData = ''.join(outputLines)   
 
-
-
-#with open('')
-
-
-l1 = [2, 5, 3, 4, 1, 0]
-l2 = [3, 1, 4, 2, 0, 5]
-
-res1 = map(fib, l1)
-res2 = map(fact, l2)
-
-print(list(res1))
-print(list(res2))
+with open('output.dat', 'w') as outputFile:
+    outputFile.write(outputData)
