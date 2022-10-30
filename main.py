636ab19b51c01340019a9a3faa6018295a8460c6
Henrique Sander Lourenço <henrique.sander@i-sensi.io>
2022-10-30 17:51:48 -0300

    Remove lisp files and add comments to the code

--- main.py
+++ main.py
@@ -3,32 +3,52 @@ Henrique Sander Lourenco
 10802705
 """
 
+# Factorial function (recursive)
 def fact(x):
     return x * fact(x - 1) if x > 1 else 1
 
+# Return term of index x of Fibonacci sequence
 def fib(x):
     return fib(x - 1) + fib(x - 2) if x > 1 else 1 if x == 1 else 0
 
+# Function to remove new line characters and spaces from list
 def isNumber(listItem):
     return True if (listItem != '\n') and (listItem != ' ') else False
 
+# Function to select only numbers which indexes are even
 def indexIsEven(listItem):
     return True if (listItem[0] % 2 == 0) else False
 
+# Function to select only numbers which indexes are odd
 def indexIsOdd(listItem):
     return True if (listItem[0] % 2 != 0) else False
 
+# Read input file
 with open('input.dat', 'r') as inputFile:
     inputData = list(inputFile.read())
 
+# Remove spaces and new line characters from inputData and transform result into a list
 filteredInputData = list(filter(isNumber, inputData))
-numberInputData =  list(enumerate(map(int, filteredInputData)))
-evenIndexData = [data[1] for data in list(filter(indexIsEven, numberInputData))]
-oddIndexData = [data[1] for data in list(filter(indexIsOdd, numberInputData))]
-fibonacci = list(map(fib, evenIndexData))
-factorial = list(map(fact, oddIndexData))
-outputLines = ["Linha {}:\tfibonacci: {}\tfactorial: {}\n".format(i, fibonacci[i], factorial[i]) for i in range(len(fibonacci))]
-outputData = ''.join(outputLines)   
 
+# Cast filteredInputData elements to int type, enumerate result and transform it into a list
+numberInputData =  list(enumerate(map(int, filteredInputData)))
+
+# Get numbers which index in enumeration is even, we want to get Fibonacci term with this numbers
+evenIndexData = [data[1] for data in list(filter(indexIsEven, numberInputData))]
+
+# Get numbers which index in enumeration is odd, we want to get the factorial of this numbers
+oddIndexData = [data[1] for data in list(filter(indexIsOdd, numberInputData))]
+
+# Get Fibonacci terms and transform result into a list
+fibonacci = list(map(fib, evenIndexData))
+
+# Get factorial of the numbers and transform result into a list
+factorial = list(map(fact, oddIndexData))
+
+# Create output data
+outputLines = ["Linha {}:\tfibonacci: {}\tfactorial: {}\n".format(i, fibonacci[i], factorial[i]) for i in range(len(fibonacci))]
+outputData = ''.join(outputLines)
+
+# Write output file
 with open('output.dat', 'w') as outputFile:
     outputFile.write(outputData)
